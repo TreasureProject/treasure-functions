e1dabeeb234018249ea4205f17bd6137c9b00db4
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-07 11:11:28 -0800

    fix verifyGenesisLegionHolders handler (#3)
    * add debug statement
    
    * parse handler body
    
    * normalize wallet inputs

--- src/handlers/bridgeworld.js
+++ src/handlers/bridgeworld.js
@@ -12,12 +12,11 @@ exports.getMines = getMines;
 exports.getLegionHolders = getLegionHolders;
 
 exports.verifyGenesisLegionHolders = async (event) => {
-  console.log(event, event?.body, event?.body?.wallet);
-  const wallets = event?.body?.wallets
-    ? event.body.wallets
-    : event?.body?.wallet
-      ? [event.body.wallet]
-      : [];
+  const body = JSON.parse(event.body);
+  const wallets = (body.wallets || body.wallet || []).map((wallet) =>
+    wallet.toLowerCase()
+  );
+  console.log("Querying Genesis Legion holder status for wallets:", wallets);
   return {
     success: await hasGenesisLegion(wallets),
   };

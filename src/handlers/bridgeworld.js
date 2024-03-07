6e278fbc456f0206689c6a853c10d9a18564bf90
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-07 13:24:28 -0800

    add verify harvester endpoint

--- src/handlers/bridgeworld.js
+++ src/handlers/bridgeworld.js
@@ -3,6 +3,7 @@ const {
   getMines,
   getLegionHolders,
   hasGenesisLegion,
+  hasHarvesterAccess,
 } = require("../services/bridgeworld");
 
 exports.getCorruption = getCorruption;
@@ -19,5 +20,17 @@ exports.verifyGenesisLegionHolders = async (event) => {
   console.log("Querying Genesis Legion holder status for wallets:", wallets);
   return {
     success: await hasGenesisLegion(wallets),
+  };
+};
+
+exports.verifyHarvesterAccess = async (event) => {
+  const body = JSON.parse(event.body);
+  const wallets = (body.wallets || body.wallet || []).map((wallet) =>
+    wallet.toLowerCase()
+  );
+  const id = event.pathParameters.id.toLowerCase();
+  console.log("Querying Harvester access for wallets:", id, wallets);
+  return {
+    success: await hasHarvesterAccess(id, wallets),
   };
 };

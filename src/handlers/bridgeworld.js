1943612f3be24f1455f7a99c4f60ebbf05831d6a
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-07 10:29:57 -0800

    add genesis legions verifier

--- src/handlers/bridgeworld.js
+++ src/handlers/bridgeworld.js
@@ -2,6 +2,7 @@ const {
   getCorruption,
   getMines,
   getLegionHolders,
+  hasGenesisLegion,
 } = require("../services/bridgeworld");
 
 exports.getCorruption = getCorruption;
@@ -9,3 +10,14 @@ exports.getCorruption = getCorruption;
 exports.getMines = getMines;
 
 exports.getLegionHolders = getLegionHolders;
+
+exports.verifyGenesisLegionHolders = async (event) => {
+  const wallets = event?.body?.wallets
+    ? event.body.wallets
+    : event?.body?.wallet
+      ? [event.body.wallet]
+      : [];
+  return {
+    success: await hasGenesisLegion(wallets),
+  };
+};

91ea8994bb4e7107ae589094bf64bbba12b828f8
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-03-07 10:44:28 -0800

    add debug statement

--- src/handlers/bridgeworld.js
+++ src/handlers/bridgeworld.js
@@ -12,6 +12,7 @@ exports.getMines = getMines;
 exports.getLegionHolders = getLegionHolders;
 
 exports.verifyGenesisLegionHolders = async (event) => {
+  console.log(event, event?.body, event?.body?.wallet);
   const wallets = event?.body?.wallets
     ? event.body.wallets
     : event?.body?.wallet

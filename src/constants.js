c4777ebfb11640e0bc9d5fdeeb80f93ce4f502a9
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-06-04 06:53:23 +0200

    run formatting

--- src/constants.js
+++ src/constants.js
@@ -83,28 +83,66 @@ module.exports = {
     ],
   },
   CIRCULATING_SUPPLY_EXCLUDED_EXTENDED: {
-    "Treasure | LP Reward Deployer (L2)": ["0x1a9c20e2b0aC11EBECbDCA626BBA566c4ce8e606",],
+    "Treasure | LP Reward Deployer (L2)": [
+      "0x1a9c20e2b0aC11EBECbDCA626BBA566c4ce8e606",
+    ],
     "Treasure | Treasury (L2)": ["0x0eB5B03c0303f2F47cD81d7BE4275AF8Ed347576"],
-    "Treasure | Marketplace (L2)": ["0xDb6Ab450178bAbCf0e467c1F3B436050d907E233",],
+    "Treasure | Marketplace (L2)": [
+      "0xDb6Ab450178bAbCf0e467c1F3B436050d907E233",
+    ],
     "Treasure | Developer (L2)": ["0xE8409cd2aBae06871D166E808D75aDdb0537033A"],
-    "Treasure | Smol Treasury (L2)": ["0x674295B530A1F69a4Bc217FFFB7E8BcDF9971678",],
-    "Treasure | Community Grants (L2)": ["0x1054E9D9091dC55a1738F9c8Fc0c79E59E222804",],
-    "Treasure | Community Gamification Fund (L2)": ["0x3Fe5d6AE3470b2De09eD062Ac78444E44Aa0cACe",],
-    "Treasure | Community Ecosystem Fund (L2)": ["0x45236EB7C47a68aE63f62F7e38f7C2F864f2Ad14",],
-    "Treasure | Master of Coin (L2)": ["0x3563590E19d2B9216E7879D269a04ec67Ed95A87",],
-    "Bridgeworld | Treasury (L2)": ["0xf9E197Aa9fa7C3b27A1A1313CaD5851B55F2FD71",],
-    "Bridgeworld | Atlas Mine (L2)": ["0xA0A89db1C899c49F98E6326b764BAFcf167fC2CE",],
-    "Bridgeworld | Harvester (Kameji) (L2)": ["0xdf9f9ca6ee5c3024b64dcecbadc462c0b896147a",],
-    "Bridgeworld | Harvester (Shinoba) (L2)": ["0x2b1de6d22e6cb9178b3ecbcb7f20b62fcce925d4",],
-    "Bridgeworld | Harvester (Asiterra) (L2)": ["0x88bf661446c8f5a7072c0f75193dae0e18ae40bc",],
-    "Bridgeworld | Harvester (Lupus Magus) (L2)": ["0x3fbfcdc02f649d5875bc9f97281b7ef5a7a9c491",],
-    "Bridgeworld | Harvester (Afarit) (L2)": ["0x70a75ac9537f6cdac553f82b6e39484acc521067",],
-    "Bridgeworld | Harvester (Emerion) (L2)": ["0x587dc30014e10b56907237d4880a9bf8b9518150",],
-    "Bridgeworld | Harvester (Thundermane) (L2)": ["0x25d6a1e968bdbebf444997286de7137df1490328",],
-    "Bridgeworld | Harvester (Emberwing) (L2)": ["0x36882e71D11EADD9F869b0fD70d18D5045939986",],
-    "Bridgeworld Game | Treasury (L1)": ["0xC983AA839910e7fa206dbC9478d9e3bbDFb88515",],
+    "Treasure | Smol Treasury (L2)": [
+      "0x674295B530A1F69a4Bc217FFFB7E8BcDF9971678",
+    ],
+    "Treasure | Community Grants (L2)": [
+      "0x1054E9D9091dC55a1738F9c8Fc0c79E59E222804",
+    ],
+    "Treasure | Community Gamification Fund (L2)": [
+      "0x3Fe5d6AE3470b2De09eD062Ac78444E44Aa0cACe",
+    ],
+    "Treasure | Community Ecosystem Fund (L2)": [
+      "0x45236EB7C47a68aE63f62F7e38f7C2F864f2Ad14",
+    ],
+    "Treasure | Master of Coin (L2)": [
+      "0x3563590E19d2B9216E7879D269a04ec67Ed95A87",
+    ],
+    "Bridgeworld | Treasury (L2)": [
+      "0xf9E197Aa9fa7C3b27A1A1313CaD5851B55F2FD71",
+    ],
+    "Bridgeworld | Atlas Mine (L2)": [
+      "0xA0A89db1C899c49F98E6326b764BAFcf167fC2CE",
+    ],
+    "Bridgeworld | Harvester (Kameji) (L2)": [
+      "0xdf9f9ca6ee5c3024b64dcecbadc462c0b896147a",
+    ],
+    "Bridgeworld | Harvester (Shinoba) (L2)": [
+      "0x2b1de6d22e6cb9178b3ecbcb7f20b62fcce925d4",
+    ],
+    "Bridgeworld | Harvester (Asiterra) (L2)": [
+      "0x88bf661446c8f5a7072c0f75193dae0e18ae40bc",
+    ],
+    "Bridgeworld | Harvester (Lupus Magus) (L2)": [
+      "0x3fbfcdc02f649d5875bc9f97281b7ef5a7a9c491",
+    ],
+    "Bridgeworld | Harvester (Afarit) (L2)": [
+      "0x70a75ac9537f6cdac553f82b6e39484acc521067",
+    ],
+    "Bridgeworld | Harvester (Emerion) (L2)": [
+      "0x587dc30014e10b56907237d4880a9bf8b9518150",
+    ],
+    "Bridgeworld | Harvester (Thundermane) (L2)": [
+      "0x25d6a1e968bdbebf444997286de7137df1490328",
+    ],
+    "Bridgeworld | Harvester (Emberwing) (L2)": [
+      "0x36882e71D11EADD9F869b0fD70d18D5045939986",
+    ],
+    "Bridgeworld Game | Treasury (L1)": [
+      "0xC983AA839910e7fa206dbC9478d9e3bbDFb88515",
+    ],
     "Smolverse | Treasury (L2)": ["0x674295B530A1F69a4Bc217FFFB7E8BcDF9971678"],
-    "Darkbright | Treasury (L1)": ["0xFc4aA8C3Ee7d40C4C38ad5565Da7AF8Fc53F1487",],
+    "Darkbright | Treasury (L1)": [
+      "0xFc4aA8C3Ee7d40C4C38ad5565Da7AF8Fc53F1487",
+    ],
   },
   MINE_NAME_MAPPING: {
     "0xdf9f9ca6ee5c3024b64dcecbadc462c0b896147a": "Kameji",

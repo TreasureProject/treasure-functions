bca3f02dc45ba7b41148af5115f10dbdb1ae959c
Alec Ananian <1013230+alecananian@users.noreply.github.com>
2024-12-11 22:29:47 +0100

    remove tdk dependencies

--- src/utils/provider.js
+++ src/utils/provider.js
@@ -1,6 +1,27 @@
-const { TREASURE_CHAIN_DEFINITION } = require("@treasure-dev/tdk-core");
 const { createPublicClient, http, fallback, defineChain } = require("viem");
 const { mainnet, arbitrum } = require("viem/chains");
+
+const TREASURE_CHAIN_DEFINITION = {
+  id: 61166,
+  name: "Treasure",
+  nativeCurrency: {
+    name: "MAGIC",
+    symbol: "MAGIC",
+    decimals: 18,
+  },
+  rpcUrls: {
+    default: {
+      http: ["https://rpc.treasure.lol"],
+    },
+  },
+  blockExplorers: {
+    default: {
+      name: "Treasure Explorer",
+      url: "https://treasurescan.io",
+      apiUrl: "https://treasurescan.io/api/v2",
+    },
+  },
+};
 
 exports.treasureClient = createPublicClient({
   chain: defineChain(TREASURE_CHAIN_DEFINITION),

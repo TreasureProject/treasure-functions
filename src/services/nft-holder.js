89a8e67b054c6e309a565cf71aa713397eddd51a
Antlion12 <96077947+Antlion12@users.noreply.github.com>
2025-02-11 12:19:59 -0800

    Include The Beacon WOP staking check for /verify-nft-holder/{address} endpoint. (#11)
    * Include The Beacon WOP staking check for /verify-nft-holder/{address} endpoint.
    
    * Allow for loop to continue even if one of the entries yields false.

--- src/services/nft-holder.js
+++ src/services/nft-holder.js
@@ -12,5 +12,25 @@ exports.hasNft = async (address, wallets) => {
     )
   );
 
-  return inventories.flat().length > 0;
+  if (inventories.flat().length > 0) {
+    return true;
+  }
+
+  // If not in the user's inventory, fallback to handling special cases.
+  if (address === "0xde20a0a37e9f1120c5ec88081185804b66c2dc7b") {
+    // Whether their WOP is staked on Treasure chain.
+    for (let i = 0; i < wallets.length; i += 1) {
+      const wallet = wallets[i];
+      const response = await fetch(
+        `https://trove-api.treasure.lol/partner/the-beacon/wop-staking/${wallet}`
+      );
+      const result = await response.json();
+      if (result?.staked) {
+        return true;
+      }
+    }
+  }
+
+  // Default to false.
+  return false;
 };
